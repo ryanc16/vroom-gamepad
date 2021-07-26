f28f23ab60c5682a64080e9ce99b78ea81808d31
ryanc16 <rconklin1994@yahoo.com>
2021-07-26 04:07:09 -0400

    Create driving hud with steering, throttle and braking

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
+import DrivingHud from './components/driving-hud/driving-hud';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+  render() {
+    return (
+      <DrivingHud />
+    );
+  }
 }
-
-export default App;
---
+++ src/components/driving-hud/driving-hud.tsx
@@ -0,0 +1,84 @@
+import { Component, Fragment } from 'react';
+import './driving-hud.css';
+import Gamepad from 'react-gamepad';
+import Throttle from '../throttle/throttle';
+import Steering from '../steering/steering';
+
+export default class DrivingHud extends Component<any, DrivingHudState> {
+
+  state = {
+    throttle: 0,
+    braking: 0,
+    steering: {
+      axisY: 0,
+      axisX: 0
+    }
+  };
+
+  render() {
+    return (
+      <Fragment>
+        <Gamepad onAxisChange={this.onAxisChangeHandler}><div/></Gamepad>
+        <div className="driving-hud">
+          <div>
+            <Steering axisX={this.state.steering.axisX} axisY={this.state.steering.axisY} />
+          </div>
+          <div className="flex-col flex-end">
+            <Throttle percent={this.state.braking} />
+            <br/>
+            <div>Braking</div>
+          </div>
+          <div className="flex-col flex-end">
+            <Throttle percent={this.state.throttle} />
+            <br/>
+            <div>Throttle</div>
+          </div>
+        </div>
+      </Fragment>
+    )
+  }
+
+  onAxisChangeHandler = (axisName: string, value: number) => {
+    if (axisName === 'RightTrigger') {
+      this.setState({throttle: value});
+    } else if (axisName === 'LeftTrigger') {
+      this.setState({braking: value});
+    } else if (axisName === 'LeftStickX') {
+      this.setState((previousState, props) => {
+        return {
+          steering: {
+            ...previousState.steering,
+            axisX: value
+          }
+        };
+      });
+    } else if (axisName === 'LeftStickY') {
+      this.setState((previousState, props) => {
+        return {
+          steering: {
+            ...previousState.steering,
+            axisY: value
+          }
+        };
+      });
+    }
+    // else if (axisName === 'LeftStickY') {
+    //   if (value >= 0) {
+    //     this.setState({throttle: value});
+    //   }
+    //   if (value <= 0) {
+    //     this.setState({braking: value*-1});
+    //   }
+    // }
+  }
+
+}
+
+interface DrivingHudState {
+  throttle: number,
+  braking: number,
+  steering: {
+    axisY: number,
+    axisX: number
+  }
+}
