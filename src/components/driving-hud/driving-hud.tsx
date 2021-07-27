59819508320cbd67fab205cb372c9d1ff3509052
ryanc16 <rconklin1994@yahoo.com>
2021-07-27 12:34:45 -0400

    added custom controls to driving hud

--- src/components/driving-hud/driving-hud.tsx
+++ src/components/driving-hud/driving-hud.tsx
@@ -12,25 +12,36 @@ export default class DrivingHud extends Component<any, DrivingHudState> {
     steering: {
       axisY: 0,
       axisX: 0
-    }
+    },
+    customControls: [
+      {button: 'A', name: 'BOOST', down: false},
+      {button: 'X', name: 'HANDBRAKE', down: false}
+    ]
   };
 
   render() {
     return (
       <Fragment>
-        <Gamepad onAxisChange={this.onAxisChangeHandler}><div/></Gamepad>
-        <div className="driving-hud flex-col">
-          <div>
-            <Steering axisX={this.state.steering.axisX} axisY={this.state.steering.axisY} />
-          </div>
-          <div className="flex-row flex-evenly">
-            <div className="flex-col flex-center">
-              <Pedal percent={this.state.braking} />
-              <div>Braking</div>
+        <Gamepad onAxisChange={this.onAxisChangeHandler} onButtonChange={this.onButtonChangeHandler}><div/></Gamepad>
+        <div className="driving-hud flex-row">
+          <div className="flex-col">
+            <div>
+              <Steering axisX={this.state.steering.axisX} axisY={this.state.steering.axisY} />
             </div>
-            <div className="flex-col flex-center">
-              <Pedal percent={this.state.throttle} />
-              <div>Throttle</div>
+            <div className="flex-row flex-evenly">
+              <div className="flex-col flex-center">
+                <Pedal percent={this.state.braking} />
+                <div>Braking</div>
+              </div>
+              <div className="flex-col flex-center">
+                <Pedal percent={this.state.throttle} />
+                <div>Throttle</div>
+              </div>
+            </div>
+          </div>
+          <div className="flex-row">
+            <div className="custom-buttons">
+              {this.printCustomControls()}
             </div>
           </div>
         </div>
@@ -38,7 +49,7 @@ export default class DrivingHud extends Component<any, DrivingHudState> {
     )
   }
 
-  onAxisChangeHandler = (axisName: string, value: number) => {
+  private onAxisChangeHandler = (axisName: string, value: number) => {
     if (axisName === 'RightTrigger') {
       this.setState({throttle: value});
     } else if (axisName === 'LeftTrigger') {
@@ -62,14 +73,22 @@ export default class DrivingHud extends Component<any, DrivingHudState> {
         };
       });
     }
-    // else if (axisName === 'LeftStickY') {
-    //   if (value >= 0) {
-    //     this.setState({throttle: value});
-    //   }
-    //   if (value <= 0) {
-    //     this.setState({braking: value*-1});
-    //   }
-    // }
+  }
+
+  private onButtonChangeHandler = (buttonName:string, down: boolean) => {
+    const idx = this.state.customControls.findIndex((custom => custom.button === buttonName));
+    if (idx >= 0) {
+      this.setState((previousState, props) => {
+        previousState.customControls[idx].down = down;
+        return previousState;
+      });
+    }
+  }
+
+  printCustomControls() {
+    return this.state.customControls.map(custom => {
+      return <div className="custom-control flex-row flex-start align-center"><span key={custom.button} style={{backgroundColor: (custom.down ? 'red' : 'transparent'), border: 'solid 1px gray', borderRadius: '50%', height: '20px', width: '20px', display: 'inline-block'}}></span>{custom.name}</div>
+    });
   }
 
 }
@@ -80,5 +99,10 @@ interface DrivingHudState {
   steering: {
     axisY: number,
     axisX: number
-  }
+  },
+  customControls: {
+    button: string,
+    name: string,
+    down: boolean
+  }[]
 }
